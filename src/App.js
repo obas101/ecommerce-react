810f738ebf089af23a8f406332296b8c593eaa6f
Obadoba Yunusa <obadoba.yunusa@e4email.net>
2022-06-17 15:31:31 +0100

    added collections to the shop page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+// import CollectionPreview from "./components/preview-collection/preview-collection.component";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route path="/shop" component={ShopPage} />
+				{/* <Route path="/collection" component={CollectionPreview} /> */}
 			</Switch>
 		</div>
 	);
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 import "./preview-collection.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
@@ -6,11 +7,12 @@ const CollectionPreview = ({ title, items }) => (
 		<h1 className="title">{title.toUpperCase()}</h1>
 		<div className="preview">
 			{items
-				.filter((items, idx) => idx < 5)
-				.map((item) => (
-					<div key={item.id}>{item.name}</div>
+				.filter((item, idx) => idx < 4)
+				.map(({ id, ...otheritemProps }) => (
+					<CollectionItem key={id} {...otheritemProps} />
 				))}
 		</div>
 	</div>
 );
+
 export default CollectionPreview;
