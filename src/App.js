c6a1e9b43d3dbceaeabdd724fd9b0d4260838e45
Obadoba Yunusa <obadoba.yunusa@e4email.net>
2022-06-17 12:16:26 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const Shoes = () => {
+	return (
+		<div>
+			<h1>THis is a shoes page</h1>
+		</div>
+	);
+};
 
 function App() {
 	return (
 		<div>
-			<Homepage />
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route path="/shoes" component={Shoes} />
+			</Switch>
 		</div>
 	);
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,24 +11,28 @@ class Directory extends React.Component {
 				{
 					id: 1,
 					title: "SHOES",
+					linkUrl: "shoes",
 					imageUrl:
 						"https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
 				},
 				{
 					id: 2,
 					title: "SHIRTS",
+					linkUrl: "",
 					imageUrl:
 						"https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hpcnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
 				},
 				{
 					id: 3,
 					title: "TROUSERS",
+					linkUrl: "",
 					imageUrl:
 						"https://images.unsplash.com/photo-1604176354204-9268737828e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJvdXNlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
 				},
 				{
 					id: 4,
 					title: "MEN",
+					linkUrl: "",
 					size: "large",
 					imageUrl:
 						"https://media.istockphoto.com/photos/headshot-portrait-of-smiling-biracial-man-posing-picture-id1292475584?b=1&k=20&m=1292475584&s=170667a&w=0&h=jZ6etD3dfOV_NIgwR9hIQ5B9TBC5kpYxShIsaqjWbzc=",
@@ -37,6 +41,7 @@ class Directory extends React.Component {
 					id: 5,
 					title: "WOMEN",
 					size: "large",
+					linkUrl: "",
 					imageUrl:
 						"https://media.istockphoto.com/photos/beautiful-girl-with-curly-hairstyle-picture-id1289362630?b=1&k=20&m=1289362630&s=170667a&w=0&h=n6Hp9-580jurTD8plgB4qVkI2-j6rZMYL-MXxIbd6Ng=",
 				},
@@ -46,8 +51,8 @@ class Directory extends React.Component {
 	render() {
 		return (
 			<div className="directory-menu">
-				{this.state.sections.map(({ id, title, imageUrl, size }) => (
-					<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+				{this.state.sections.map(({ id, ...otherSection }) => (
+					<MenuItem key={id} {...otherSection} />
 				))}
 			</div>
 		);
