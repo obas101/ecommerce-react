cde55990864f5b368fa4cbfab0f563278e290090
Obadoba Yunusa <obadoba.yunusa@e4email.net>
2022-06-17 13:42:47 +0100

    create shop items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
-
-const Shoes = () => {
-	return (
-		<div>
-			<h1>THis is a shoes page</h1>
-		</div>
-	);
-};
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={Homepage} />
-				<Route path="/shoes" component={Shoes} />
+				<Route path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
