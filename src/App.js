b5ee11bf78a91f82c0c51fe4e5ee3537e7ebce6e
Obadoba Yunusa <obadoba.yunusa@e4email.net>
2022-06-19 00:21:38 +0200

    added signup change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 // import CollectionPreview from "./components/preview-collection/preview-collection.component";
@@ -11,6 +12,7 @@ function App() {
 			<Header />
 			<Switch>
 				<Route exact path="/" component={Homepage} />
+				<Route path="/signin" component={SignInAndSignUpPage} />
 				<Route path="/shop" component={ShopPage} />
 				{/* <Route path="/collection" component={CollectionPreview} /> */}
 			</Switch>
